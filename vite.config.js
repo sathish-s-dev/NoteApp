3ea0813f6b7f230a27d70391ef7450fff3aa8041
Sathish S <s.sathishmechddesign@gmail.com>
2023-08-21 15:44:24 +0530

    nothing

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
+export default defineConfig(() => {
 	const config = {
 		plugins: [react()],
 		base: '/NoteApp/',
 	};
-
-	if (command !== 'serve') {
-		config.base = '/react-vite-gh-pages/';
-	}
 
 	return config;
 });
